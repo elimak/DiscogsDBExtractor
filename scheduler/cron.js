3bba24603e3beb946d7b6c1ae48dabaf2e602d53
elimak <info@elimak.com>
2016-08-14 22:21:10 -0700

    Update: test remotly

--- scheduler/cron.js
+++ scheduler/cron.js
@@ -81,7 +81,7 @@ function _process() {
 
 
 new CronJob({
-    cronTime: '20 * * * *', // 15 seconds after every minute
+    cronTime: '24 * * * *', // 15 seconds after every minute
     //cronTime: '1 */6 * * *', // 2 times a day
     //onTick: processRelease,
     //onTick: loadResource,
